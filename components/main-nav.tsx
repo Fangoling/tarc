6847a5c41c911486a6601ff015e191ce27122108
Fangxing Liu <ge83toc@mytum.de>
2024-09-07 01:19:49 +0200

    update: refactored the navbar
    - navbar code is now more readable
    - uses lucide icon instead of v0 generated icon
    - hamburger now works on mobile, see commit 'fixed: sheet'

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -4,8 +4,9 @@ import * as React from "react"
 import Link from "next/link"
 import Img from "next/image"
 
+import { Menu } from "lucide-react"
 import { cn } from "@/lib/utils"
-import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet"
+import { Sheet, SheetTrigger, SheetContent, SheetClose } from "@/components/ui/sheet"
 import { Button } from "@/components/ui/button"
 import { ModeToggle } from "./ui/mode-toggle"
 import {
@@ -26,7 +27,21 @@ const services: { title: string; href: string; description: string }[] = [
   }
 ]
 
+const navbarItems: 
+{
+  name: string;
+  href: string;
+  subcontent?: {title: string, href: string, description: string}[]
+}[] = [
+  {name: 'Home', href: '/'},
+  {name: 'Services', href: '/', subcontent: services},
+  {name: 'Projects', href: '/#projects'},
+  {name: 'Team', href: '/#team'},
+  {name: 'Contact', href: '/#contact'}
+]
+
 export default function Nav() {
+
   return (
     <div className="flex items-center justify-between px-4 py-2 pb-4">
       <Link href="/" className="flex items-center gap-2">
@@ -51,112 +66,74 @@ export default function Nav() {
 
       <NavigationMenu className="hidden md:flex">
         <NavigationMenuList>
-
-          <NavigationMenuItem>
-            <Link href="/" className="text-lg font-medium" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Home
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Services</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid w-[200px] gap-3 p-4 md:w-[300px] md:grind-cols-2 lg:w-[400px]">
-                {services.map((service) => (
-                  <ListItem
-                    key={service.title}
-                    title={service.title}
-                    href={service.href}
-                  >
-                    {service.description}
-                  </ListItem>
-                ))}
-                </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-
-          <NavigationMenuItem>
-            <Link href="/#projects" className="text-lg font-medium" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Projects
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-
-          <NavigationMenuItem>
-            <Link href="/#team" className="text-lg font-medium" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Team
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          
-          <NavigationMenuItem>
-            <Link href="/#contact" className="text-lg font-medium" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Contact
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
+          {navbarItems.map(({ name, href, subcontent }) => (
+            !subcontent ? 
+            <NavigationMenuItem key={name}>
+              <Link href={href} className="text-lg font-medium" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  {name}
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            : 
+            <NavigationMenuItem key={name}>
+              <NavigationMenuTrigger>{name}</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid w-[200px] gap-3 p-2 md:w-[300px] md:grind-cols-2 lg:w-[400px]">
+                  {subcontent.map(({ title, href, description }) => (
+                    <ListItem
+                      key={title}
+                      title={title}
+                      href={href}
+                    >
+                      {description}
+                    </ListItem>
+                  ))}
+                  </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          ))}
 
         </NavigationMenuList>
       </NavigationMenu>
 
       <div className="flex items-center gap-2">
         <ModeToggle/>
-        <Sheet>
+        <Sheet> 
           <SheetTrigger asChild>
             <Button variant="outline" size="icon" className="md:hidden">
-              <MenuIcon className="h-6 w-6" />
+              <Menu />
               <span className="sr-only">Toggle navigation menu</span>
             </Button>
           </SheetTrigger>
           <SheetContent side="right">
-            <div className="grid w-[200px] p-4">
-              <Link href="/" className="text-lg font-medium hover:underline underline-offset-4" prefetch={false}>
-                Home
-              </Link>
-              <Link href="/#services" className="text-lg font-medium hover:underline underline-offset-4" prefetch={false}>
-                Services
-              </Link>
-              <Link href="/#projects" className="text-lg font-medium hover:underline underline-offset-4" prefetch={false}>
-                Projects
-              </Link>
-              <Link href="/#team" className="text-lg font-medium hover:underline underline-offset-4" prefetch={false}>
-                Team
-              </Link>
-              <Link href="/#contact" className="text-lg font-medium hover:underline underline-offset-4" prefetch={false}>
-                Contact
-              </Link>
+            <div className="flex flex-col items-start w-[400px] p-4 gap-2">
+              {navbarItems.map(({name, href, subcontent}) => (
+                !subcontent ? 
+                  <SheetClose key={name}>
+                    <Link key={name} href={href} className="text-4xl font-medium hover:underline" prefetch={false}>
+                      {name}
+                    </Link>
+                  </SheetClose> 
+                :
+                <div key={name}>
+                  <p className="text-4xl font-medium">{name + ":"}</p>
+                  {subcontent.map(({title, href}) => (
+                    <SheetClose key={title}>
+                      <Link key={title} href={href} className="text-4xl font-medium hover:underline" prefetch={false}>
+                        {"- " + title}
+                      </Link>
+                    </SheetClose> 
+                  ))}
+                </div>
+
+                ))}
             </div>
           </SheetContent>
         </Sheet>
 
       </div>
     </div>
-  )
-}
-
-function MenuIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <line x1="4" x2="20" y1="12" y2="12" />
-      <line x1="4" x2="20" y1="6" y2="6" />
-      <line x1="4" x2="20" y1="18" y2="18" />
-    </svg>
   )
 }
 
