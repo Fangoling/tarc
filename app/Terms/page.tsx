c5dd98a71383f8d267e843a6a017c1530b687141
Fangxing Liu <ge83toc@mytum.de>
2024-09-07 01:16:51 +0200

    fix: Terms page was not accessible

--- app/Terms/page.tsx
+++ app/Terms/page.tsx
@@ -1,7 +1,9 @@
+import "@/styles/globals.css" 
 export default function Terms() {
     return (
-        <>
+        <div className="mx-8">
             <h1>Terms of Service</h1>
-        </>
+            <p>Last Updated: 07.09.2024</p>
+        </div>
     )
 }
