8354f75a6422bc3b1ca389a22decf1b289f71a65
Fangxing Liu <ge83toc@mytum.de>
2024-09-08 23:56:23 +0200

    auth: see description of last auth commit
    tldr: display userinfo on protected page, signout action, forgotpassword and resetpassword action

--- app/actions.ts
+++ app/actions.ts
@@ -48,8 +48,6 @@ export const signInAction = async (formData: FormData) => {
     const password = formData.get("password") as string 
     const supabase = createClient()
 
-    console.log("hello")
-
     const { error } = await supabase.auth.signInWithPassword({
         email,
         password,
@@ -95,4 +93,47 @@ export const forgotPasswordAction = async (formData: FormData) => {
         "/forgot-password",
         "Check your email for a link to reset your password."
     )
+}
+
+export const resetPasswordAction = async (formData: FormData) => {
+    const supabase = createClient()
+
+    const password = formData.get("password") as string
+    const confirmPassword = formData.get("confirmPassword") as string
+
+    if (!password || !confirmPassword) {
+        return encodedRedirect(
+            "error",
+            "/protected/reset-password",
+            "Password and confirm password are required"
+        )
+    }
+
+    if (password !== confirmPassword) {
+        return encodedRedirect(
+            "error",
+            "/protected/reset-password",
+            "Passwords do not match"
+        )
+    }
+
+    const { error } = await supabase.auth.updateUser({
+        password: password,
+    })
+
+    if (error) {
+        encodedRedirect(
+            "error",
+            "/protected/reset-password",
+            "update failed"
+        )
+    }
+
+    encodedRedirect("success", "/protected/reset-password", "Password updated")
+}
+
+export const signOutAction = async () => {
+    const supabase = createClient()
+    await supabase.auth.signOut()
+    return redirect("/sign-in")
 }
--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,10 +1,40 @@
-import { createClient } from "@/lib/utils/supabase/client"
+import { createClient } from "@/lib/utils/supabase/server"
+import { signOutAction } from "@/app/actions"
+import { redirect } from "next/navigation"
 
-export default function LoginSuccess() {
+import { Button } from "@/components/ui/button"
+
+export default async function LoginSuccessPage() {
+    const supabase = createClient()
+
+    const {
+        data: { user },
+    } = await supabase.auth.getUser()
+
+    if (!user) {
+        return redirect("/sign-in")
+    }
+
 
     return (
-        <div className="flex items-start justify-center mx-4">
-            <h1>You can only access this page if you are logged in. Congrats.</h1>
+        <div className="flex items-center justify-center mx-4">
+            <div className="flex flex-col gap-4 items-center">
+                <h1>You can only access this page if you are logged in.</h1>
+
+                <h2>Hi. Here are your user details</h2>
+                <div>
+                    <p>
+                        email: {user.user_metadata.email}
+                        <br />first name: {user.user_metadata.first_name}
+                        <br />last name: {user.user_metadata.last_name}
+                    </p>
+                </div>
+                <form>
+                    <Button className="max-w-[180px]" formAction={signOutAction}>
+                        Sign Out
+                    </Button>
+                </form>
+            </div>
         </div>
     )
 }
