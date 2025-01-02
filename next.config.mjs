c75d7c9155c5f207074a34f88b02def7a78c9cdc
Fangxing Liu <ge83toc@mytum.de>
2025-01-03 00:15:46 +0100

    setup redirect from tarc-fitness.de to tarc-fitness.com/de

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,22 @@ const nextConfig = {
 
             }
         ]
-    }
+    },
+    async redirects() {
+    return [
+      {
+        source: '/:path*', // Match all paths
+        has: [
+          {
+            type: 'host',
+            value: 'tarc-fitness.de', // Match requests from tarc-fitness.de only
+          },
+        ],
+        destination: 'https://tarc-fitness.com/de/:path*', // Redirect to tarc-fitness.com/de
+        permanent: true, // Use a 301 permanent redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
