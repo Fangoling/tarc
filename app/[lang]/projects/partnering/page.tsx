de4b60bd663499a1b9617c5bf8fc70c88a99aaa1
Fangxing Liu <ge83toc@mytum.de>
2025-01-05 22:13:49 +0100

    add content to the partnering page

--- app/[lang]/projects/partnering/page.tsx
+++ app/[lang]/projects/partnering/page.tsx
@@ -14,7 +14,7 @@ export default async function ResortPage({
         <div>
             <div className="relative h-[60vh] max-h-[400px] overflow-hidden">
                 <Image
-                    src="/images/shakingHands.jpg"
+                    src="/images/partnering/shakingHands.jpg"
                     alt="Madeira resort view"
                     fill
                     className="object-cover"
@@ -30,6 +30,51 @@ export default async function ResortPage({
                         </p>
                     </div>
                 </div>
+            </div>
+
+            <div className="flex flex-col items-center mx-auto w-full max-w-4xl">
+
+                <div className="container relative mx-auto px-4 py-12 lg:py-24">
+                    <p dangerouslySetInnerHTML={{__html: dict.partnering.introduction}}/>
+                    <h2>Sport Marketing</h2>
+                    <p>{dict.partnering.marketing}</p>
+                    <h2>Vision</h2>
+                    <p>{dict.partnering.vision}</p>
+                    <h2>{dict.partnering.what}</h2>
+                    <ul className="my-6 ml-6 list-decimal [&>li]:mt-2 marker:text-2xl">
+                        {dict.partnering.whatlist.map((elem, i) => (
+                            <li key={i}>
+                                <h3>{elem[0]}</h3>
+                                <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+                                {elem.slice(1).map((e, j) => (<li key={j}>{e}</li>) )}
+                                </ul>
+                            </li>
+                        ))}
+                    </ul>
+                    <h2>{dict.partnering.why}</h2>
+                    <ul className="my-6 ml-6 list-decimal [&>li]:mt-2 marker:text-2xl">
+                        {dict.partnering.whylist.map((elem, i) => (
+                            <li key={i}>
+                                <h3>{elem[0]}</h3>
+                                <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+                                {elem.slice(1).map((e, j) => (<li key={j}>{e}</li>) )}
+                                </ul>
+                            </li>
+                        ))}
+                    </ul>
+                    <h2>{dict.partnering.who}</h2>
+                    <ul className="my-6 ml-6 list-disc [&>li]:mt-2 marker:text-2xl">
+                        {dict.partnering.wholist.map((elem, i) => (
+                            <li key={i}>
+                                <p>{elem[0]}</p>
+                                <ul className="my-6 ml-6 list-disc [&>li]:mt-2 marker:text-sm">
+                                {elem.slice(1).map((e, j) => (<li key={j}>{e}</li>) )}
+                                </ul>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
             </div>
 
         </div>
