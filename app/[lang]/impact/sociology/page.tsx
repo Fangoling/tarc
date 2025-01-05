b0008404fe54e14f1f74f1b9d46aa11cb0a5a767
Fangxing Liu <ge83toc@mytum.de>
2025-01-05 22:15:16 +0100

    add new sociology page and add button redirect button from impact (also use pixabay pictures)

--- app/[lang]/impact/sociology/page.tsx
+++ app/[lang]/impact/sociology/page.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image"
-import Link from "next/link"
 import { AppData } from "@/components/prop-types"
-import { getDictionary } from "../dictionaries"
-import { Button } from "@/components/ui/button"
-export default async function ImpactPage({
+import { getDictionary } from "../../dictionaries"
+
+export default async function SociologyPage({
     params,
 }: {
     params: Promise<{ lang: 'en' | 'de' }>
@@ -16,7 +15,7 @@ export default async function ImpactPage({
             <div className="relative h-[60vh] max-h-[400px] overflow-hidden">
                 <Image
                     src="/images/impact/earth.jpg"
-                    alt="Earth picture"
+                    alt="earth picture"
                     fill
                     className="object-cover"
                 />
@@ -24,7 +23,7 @@ export default async function ImpactPage({
                 <div className="absolute inset-0 flex flex-col justify-end p-8 md:p-12">
                     <div>
                         <h1 className="text-3xl md:text-5xl font-bold text-white">
-                            {dict.impact.impact.title}
+                            {dict.impact.sociology.title}
                         </h1>
                     </div>
                 </div>
@@ -33,26 +32,17 @@ export default async function ImpactPage({
             <div className="flex flex-col items-center mx-auto w-full max-w-4xl">
 
                 <div className="container relative mx-auto px-4 py-12 lg:py-24">
-                    <p dangerouslySetInnerHTML={{__html: dict.impact.impact.text}}/>
-                </div>
-
-
-                <div className="container relative mx-auto px-4 py-12 lg:py-24">
                     <div className="grid gap-16 lg:grid-cols-2 lg:gap-12 items-center">
                         <div className="lg:order-2">
-                            <h2>{dict.impact.humanity.title}</h2>
-                            <p dangerouslySetInnerHTML={{__html: dict.impact.humanity.text}}/>
-                            <Button variant="outline" className="mt-4 text-xl p-6" asChild>
-                                <Link href="/impact/sociology">
-                                    {dict.impact.button}
-                                </Link>
-                            </Button>
+                            <h2>{dict.impact.body.title}</h2>
+                            <p dangerouslySetInnerHTML={{__html: dict.impact.body.text}}/>
                         </div>
+
                         <div className="relative h-[400px] w-full md:h-[500px] lg:order-1">
                             <div>
                                 <Image
-                                    src="/images/impact/network.jpg"
-                                    alt="connected earth"
+                                    src="/images/impact/handstand.jpg"
+                                    alt="man doing a handstand at the beach"
                                     fill
                                     className="object-cover"
                                     priority
@@ -61,8 +51,8 @@ export default async function ImpactPage({
                             <div className="absolute -bottom-12 -left-16 h-36 w-48 md:h-48 md:w-64">
                                 <div className="relative h-full w-full overflow-hidden">
                                     <Image
-                                        src="/images/impact/asia.jpg"
-                                        alt="two asian historical creatures fighting"
+                                        src="/images/impact/strongman.jpg"
+                                        alt="a man with a lot of muscle doing a squat"
                                         fill
                                         className="object-cover"
                                     />
@@ -78,15 +68,15 @@ export default async function ImpactPage({
                     <div className="grid gap-8 lg:grid-cols-2 lg:gap-12 items-center">
                         <div>
 
-                            <h2>{dict.impact.planet.title}</h2>
-                            <p dangerouslySetInnerHTML={{__html: dict.impact.planet.text}}/>
+                            <h2>{dict.impact.mind.title}</h2>
+                            <p dangerouslySetInnerHTML={{__html: dict.impact.mind.text}}/>
                         </div>
 
                         <div className="relative h-[400px] w-full md:h-[500px]">
                             <div>
                                 <Image
-                                    src="/images/impact/elephant.jpg"
-                                    alt="elephant standing in a forest"
+                                    src="/images/impact/neural.jpg"
+                                    alt="top down look on forest"
                                     fill
                                     className="object-cover"
                                     priority
@@ -95,8 +85,8 @@ export default async function ImpactPage({
                             <div className="absolute -bottom-12 -right-16 h-36 w-48 md:h-48 md:w-64">
                                 <div className="relative h-full w-full overflow-hidden">
                                     <Image
-                                        src="/images/impact/trees.jpg"
-                                        alt="two trees with sunlight shining through"
+                                        src="/images/impact/depressed.jpg"
+                                        alt="a depressed bald man behind broken glass"
                                         fill
                                         className="object-cover"
                                     />
