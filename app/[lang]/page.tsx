afb4e8a8ba018abaa8924f2eb2572ff6ca53f166
Fangxing Liu <ge83toc@mytum.de>
2025-01-05 22:14:27 +0100

    change pictures to pictures from pixabay

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,7 +1,8 @@
 import * as React from "react"
 
 import { PhoneIcon, MapPinIcon, MailIcon } from "lucide-react"
-
+import { ArrowRight } from 'lucide-react'
+import Image from "next/image"
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
 
 import { Hero } from '@/components/hero'
@@ -34,6 +35,24 @@ export default async function Home({
             <div className="flex flex-col items-center gap-20">
                 <Hero dict={dict} />
                 <ProjectSection dict={dict} />
+
+
+                <section className="flex flex-col gap-8 items-center w-full bg-gradient-to-b from-secondary/10 via-secondary to-secondary/10 pt-16 pb-24">
+                    <h1>{dict.goals}</h1>
+                    <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                        <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
+                            <iframe
+                                className="absolute top-0 left-0 w-full h-full"
+                                src="https://www.youtube.com/embed/nfYQC6tgJQI?si=CZdOisX56aDmdOXD"
+                                title="Tarc Fitness announcement"
+                                loading="lazy"
+                                referrerPolicy="strict-origin-when-cross-origin"
+                                allowFullScreen
+                            />
+                        </div>
+                    </div>
+                </section>
+
                 <section id="team" className="w-full flex flex-col items-center">
                     <div className="flex flex-col items-center w-full max-w-4xl gap-8 p-8 rounded-xl">
                         <h1>Team</h1>
@@ -49,23 +68,6 @@ export default async function Home({
                             ))}
                         </div>
 
-                    </div>
-                </section>
-
-                <section className="flex flex-col gap-8 items-center w-full bg-gradient-to-b from-secondary/10 via-secondary to-secondary/10 pt-16 pb-24">
-                    <h1>{dict.goals}</h1>
-                    <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                        <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
-                            <iframe
-                                className="absolute top-0 left-0 w-full h-full"
-                                src="https://www.youtube.com/embed/nfYQC6tgJQI?si=CZdOisX56aDmdOXD"
-                                title="YouTube video player"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                loading="lazy"
-                                referrerPolicy="strict-origin-when-cross-origin"
-                                allowFullScreen
-                            />
-                        </div>
                     </div>
                 </section>
 
