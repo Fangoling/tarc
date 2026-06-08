d50491596c15386bc46314ef0d53d8dcd1234c0d
Fangxing Liu <fx.liu@tum.de>
2026-06-08 18:08:12 +0200

    fix: update impressum

--- app/[lang]/impressum/page.tsx
+++ app/[lang]/impressum/page.tsx
@@ -5,10 +5,10 @@ export default function Terms() {
             <p>
                 Name der Firma : Tarc-Fitness GmbH
                 <br />Geschäftsführers : Finn Untiedt
-                <br />Anschrift : Diechmannplatz 1 Essen 45355 Essen, DE
+                <br />Anschrift : Hiertrieferstraße 65 Essen 45143 Essen, DE
                 <br />Telefon : +49 201 72994195
                 <br />E-Mail : info@tarc-fitness.de
-                <br />Eintragung im Amtsgericht Düsseldorf
+                <br />Eintragung im Amtsgericht Essen
                 <br />Handelsregisternummer: HRB 36471
             </p>
         </div>
